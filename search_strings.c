0878345c333d5b7f08e7ffb45f54afb31b022915
Kunal Gupta <43882773+kunal0287@users.noreply.github.com>
2019-10-13 21:33:50 +0530

    Update search_strings.c

--- search_strings.c
+++ search_strings.c
@@ -3,39 +3,40 @@
 #include<string.h>
 int main()
 {
-int i,f;
-int c=0;
-char a[500];
-char b[500];
-printf("enter the sentence to serarch a word\n");
-scanf("%[^\n]", a);
-printf("enter the word you want to search\n");
-scanf("%s", b);
-for (i=0;a[i] != '\0';i++)
-{
-    if (a[i] == b[c] && b[c] != '\0')
+    int i,f;
+    int c=0;
+    char a[500];
+    char b[500];
+    printf("enter the sentence to serarch a word\n");
+    scanf("%[^\n]", a);
+    printf("enter the word you want to search\n");
+    scanf("%s", b);
+    for (i=0;a[i] != '\0';i++)
     {
-        c++;
-        if (c==strlen(b))
+        if (a[i] == b[c] && b[c] != '\0')
         {
-            f = 1;
+            c++;
+            if (c==strlen(b))
+            {
+                f = 1;
+            }
+            else
+            {
+                f = 0;
+            }
         }
         else
         {
-            f = 0;
+            c = 0;
         }
+    }
+    if (f == 1)
+    {
+        printf("The word has found\n");
     }
     else
     {
-        c = 0;
+        printf("No result found\n");
     }
-}
-if (f == 1)
-{
-    printf("The word has found\n");
-}
-else
-{
-    printf("No result found\n");
-}
-}
+        return 0;
+    }
